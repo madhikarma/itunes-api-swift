23fed8fd71f4ec66b7ebde079243e3ca29035313
Shagun Madhikarmi <shagun@ustwo.com>
2019-11-19 06:44:19 +0000

    Unit tests: fix for false positive added a wait expectation

--- Tests/iTunesAPITests/iTunesAPITests.swift
+++ Tests/iTunesAPITests/iTunesAPITests.swift
@@ -5,7 +5,10 @@ final class iTunesAPITests: XCTestCase {
     func testGetSearchResults() {
         let searchAPI = iTunesSearchAPI()
         print(Date().timeIntervalSince1970)
+        let expectation = XCTestExpectation()
+        
         searchAPI.getResults(searchTerm: "Bonobo") { (result) in
+            expectation.fulfill()
             switch result {
             case .success(let searchResults):
                 XCTAssertTrue(searchResults.count > 0)
@@ -13,6 +16,7 @@ final class iTunesAPITests: XCTestCase {
                 XCTFail("Error: expected search results but instead there was an error: \(error)")
             }
         }
+        XCTWaiter().wait(for: [expectation], timeout: 5)
     }
 
     static var allTests = [
